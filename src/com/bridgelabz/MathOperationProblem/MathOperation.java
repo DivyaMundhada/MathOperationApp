b81b16b17947b35e857382ec8c200df443ea749e
DivyaMundhada <divyamundada9@gmail.com>
2021-07-05 12:02:37 +0530

    Added Feature to Perform Math Operations Using Lambda Expression.

--- src/com/bridgelabz/MathOperationProblem/MathOperation.java
+++ src/com/bridgelabz/MathOperationProblem/MathOperation.java
@@ -1,11 +1,24 @@
 package com.bridgelabz.MathOperationProblem;
 
+@FunctionalInterface
+interface IMathFunction {
+	int calculate(int a, int b);
+}
+
 public class MathOperation {
-	
-	//Welcome Message
+
+	// Welcome Message
 	public static void main(String[] args) {
 		System.out.println("Welcome to Math Operation App Using Lambda Expression.");
+		// This expression implements 'IMathFunction' interface
+		IMathFunction add = Integer::sum;
+		IMathFunction multiply = (x, y) -> x * y;
+		IMathFunction divide = (x, y) -> x / y;
 
+		// Add & Multiply two numbers using lambda expression
+		System.out.println("Addition is " + add.calculate(6, 3));
+		System.out.println("Multiplication is " + multiply.calculate(6, 3));
+		System.out.println("Division is " + divide.calculate(6, 3));
 	}
 
 }
