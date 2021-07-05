aad95b2e09fff458eb8b16fea0538f9eea643862
DivyaMundhada <divyamundada9@gmail.com>
2021-07-05 11:43:28 +0530

    Added Welcome Message

---
+++ src/com/bridgelabz/MathOperationProblem/MathOperation.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.MathOperationProblem;
+
+public class MathOperation {
+	
+	//Welcome Message
+	public static void main(String[] args) {
+		System.out.println("Welcome to Math Operation App Using Lambda Expression.");
+
+	}
+
+}
